baace611d2f2a39618198b49c4aefc930ebbafad
mathius johnson <johnson_m@qwasar.io>
2023-05-17 05:03:41 +0000

    chore: rewrite itoa/pitoa to custom intToString

--- my_printf.c
+++ my_printf.c
@@ -1,93 +1,60 @@
+#include <ctype.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <ctype.h>
-#include <stdint.h>
 
-int is_null_terminated(const char *str) {
-  return (strchr(str, '\0') != NULL);
-}
+int is_null_terminated(const char *str) { return (strchr(str, '\0') != NULL); }
 
-void reverse(char *str, int len) {
-  int i, j;
-  for (i = 0, j = len - 1; i < j; i++, j--) {
-    char temp = str[i];
-    str[i] = str[j];
-    str[j] = temp;
-  }
-}
+char *intToString(long num, char *str, int base) {
+  int i = 0, sign = 0;
 
-char *itoa(int value, char *str, int base) {
-  // check for valid base
-  if (base < 2 || base > 36) {
-    *str = '\0';
-    return str;
+  // check if number is negative
+  if (num < 0) {
+    sign = 1;
+    num = -num;
   }
 
-  // handle zero case
-  if (value == 0) {
-    str[0] = '0';
-    str[1] = '\0';
-    return str;
-  }
-  
-  // handle negative values
-  int is_negative = 0;
-  if (value < 0) {
-    is_negative = 1;
-    value = -value;
-  }
+  // Extract digits from the number and convert them to characters
+  do {
+    // Get the remainder of the number divided by the base
+    int digit = num % base;
 
-  // convert digits to string in reverse order
-  int i = 0;
-  while (value != 0) {
-    int rem = value % base;
-    str[i++] = (rem > 9) ? (rem - 10 + 'a') : (rem + '0');
-    value /= base;
-  }
+    // If digit < 10, convert it to a character by adding the ASCII value of '0'.
+    // The ASCII value of '0' is 48, if digit = 5, the code adds 5 + 48 to get the ASCII value of '5'.
+    if (digit < 10) {
+      str[i++] = digit + '0';
+    }
+    // If digit >= 10, convert it to a character by subtracting 10 from the digit
+    // to get the value of the second digit, and then adding the ASCII value of 'a'.
+    // The ASCII value of 'a' is 97, if digit = 10, the code subtracts 10 from digit (10 - 10 = 0)
+    // and adds 97 to get the ASCII value of 'a'.
+    else {
+      str[i++] = digit - 10 + 'a';
+    }
 
-  // add sign if necessary
-  if (is_negative) {
+    // Divide the number by the base to get the next digit
+    // Since it's an int, the decimal value is not included
+    // eg 12 / 10 = 1.2 = 1
+    num /= base;
+  } while (num > 0);
+
+  if (sign) {
     str[i++] = '-';
   }
 
-  // add null terminator and reverse the string
   str[i] = '\0';
-  reverse(str, i);
 
+  int len = i;
+  for (i = 0; i < len / 2; i++) {
+    char temp = str[i];
+    str[i] = str[len - i - 1];
+    str[len - i - 1] = temp;
+  }
   return str;
 }
-char *pitoa(uintptr_t value, char *str, int base) {
-  // check for valid base
-  if (base < 2 || base > 36) {
-    *str = '\0';
-    return str;
-  }
-
-  // handle zero case
-  if (value == 0) {
-    str[0] = '0';
-    str[1] = '\0';
-    return str;
-  }
-
-  // convert digits to string in reverse order
-  int i = 0;
-  while (value != 0) {
-    int rem = value % base;
-    str[i++] = (rem > 9) ? (rem - 10 + 'a') : (rem + '0');
-    value /= base;
-  }
-
-  // add null terminator and reverse the string
-  str[i] = '\0';
-  reverse(str, i);
-
-  return str;
-}
-
 
 int write_char(char c) { return (write(1, &c, 1)); }
 
@@ -112,91 +79,80 @@ int my_printf(const char *format, ...) {
         if (s == NULL) {
           int len = strlen("(null)");
           write(1, "(null)", len);
-            count+=len;
-            break;
+          count += len;
+          break;
         }
         if (is_null_terminated(s)) {
-            int len = strlen(s);
-
-            // printf("string found %s\n", s);
-            write(1, s, len);
-            count+=len;
-            break;
+          int len = strlen(s);
+          write(1, s, len);
+          count += len;
+          break;
         }
       case 'd': /* int */
-          d = va_arg(args, int);
-          char buffer[20];
-          itoa(d, buffer, 10);
-          int len1 = strlen(buffer);
-          write(1, buffer, len1);
-          count += len1;
-          break;
+        d = va_arg(args, int);
+        char buffer[20];
+        intToString(d, buffer, 10);
+        int len1 = strlen(buffer);
+        write(1, buffer, len1);
+        count += len1;
+        break;
       case 'c': /* char */
-        /* Note: char is promoted to int. */
+        /* char is promoted to int. */
         c = va_arg(args, int);
         write(1, &c, sizeof(char));
         count++;
         break;
-      case 'o':{ /* octal */
+      case 'o': { /* octal */
         int u = va_arg(args, unsigned int);
         char buffer[20];
-        itoa(u, buffer, 8);
+        intToString(u, buffer, 8);
         int len = strlen(buffer);
         write(1, buffer, len);
         count += len;
         break;
+      }
+      case 'u': { /* unsigned decimal */
+        int u = va_arg(args, unsigned int);
+        char buffer[20];
+        intToString(u, buffer, 10);
+        int len = strlen(buffer);
+        write(1, buffer, len);
+        count += len;
+        break;
+      }
+      case 'x': { /* unsigned decimal */
+        int u = va_arg(args, unsigned int);
+        char buffer[20];
+        intToString(u, buffer, 16); /* Use base 16 */
+        int len = strlen(buffer);
+        /* Convert string to uppercase */
+        for (int i = 0; i < len; i++) {
+          buffer[i] = toupper(buffer[i]);
         }
-        case 'u': {/* unsigned decimal */
-            int u = va_arg(args, unsigned int);
-            char buffer[20];
-            itoa(u, buffer, 10);
-            int len = strlen(buffer);
-            write(1, buffer, len);
-            count += len;
-            break;
-        }
-        case 'x': {/* unsigned decimal */
-            int u = va_arg(args, unsigned int);
-            char buffer[20];
-            itoa(u, buffer, 16); /* Use base 16 */
-            int len = strlen(buffer);
-                /* Convert string to uppercase */
-            for (int i = 0; i < len; i++) {
-                buffer[i] = toupper(buffer[i]);
-            }
-            write(1, buffer, len);
-            count += len;
-            break;
-        }
-        case 'p': {/* pointer */
-            void* p = va_arg(args, void*);
-            uintptr_t p_as_int = (uintptr_t)p;
-            char buffer[20];
-            pitoa(p_as_int, buffer, 16);
-            int len = strlen(buffer);
+        write(1, buffer, len);
+        count += len;
+        break;
+      }
+      case 'p': { /* pointer */
+        void *p = va_arg(args, void *);
+        uintptr_t p_as_int = (uintptr_t)p;
+        char buffer[20];
+        intToString(p_as_int, buffer, 16);
+        int len = strlen(buffer);
 
-            /* Prefix with "0x" to indicate memory address */
-            char output[22] = "0x";
-            strncpy(output+2, buffer, len);
-            output[len+2] = '\0';
+        /* Prefix with "0x" to indicate memory address */
+        char output[22] = "0x";
+        strncpy(output + 2, buffer, len);
+        output[len + 2] = '\0';
 
-            write(1, output, len+2);
-            count += len+2;
-            break;
-        }
+        write(1, output, len + 2);
+        count += len + 2;
+        break;
+      }
       }
     }
     i++;
   }
   va_end(args);
   return count;
-}
-
-int main(void) {
-  // my_printf("Hello %s!\n", "QWASAR.IO");
-  // my_printf("Hello %s %s!\n", "Mr", "Gaetan");
-  int ret_val = my_printf("%c%c%c%c%c!\n", 'H', 'e', 'l', 'l', 'o');
-  printf("return value %d", ret_val);
-  // my_printf("%c%c%c%c%c!\n", 'H', 'e', 'l', 'l', 'o');
-  return 0;
 }
